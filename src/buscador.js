753c9c76b567584e26f9a92599c5c7f11530b506
BetoHerbas <betitoherbas@gmail.com>
2024-05-08 21:06:23 -0400

    encuentra un proyecto cuando el mismo existe en uan lista de 1 proyecto

--- src/buscador.js
+++ src/buscador.js
@@ -1,5 +1,8 @@
 
 export default function searchProject(name, projects) {
   let foundProjects = [];
+  if(projects.length != 0) {
+    foundProjects.push(projects[0]);
+  }
   return foundProjects;
 }
--- src/buscador.test.js
+++ src/buscador.test.js
@@ -5,4 +5,8 @@ describe("buscar", () => {
     let proyectos = [];
     expect(searchProject("proyecto1", proyectos)).toEqual([]);
   });
+  it("encuentra un proyecto cuando el mismo existe en uan lista de 1 proyecto", () => {
+    let proyectos = ["proyecto1"];
+    expect(searchProject("proyecto1", proyectos)).toEqual(["proyecto1"]);
+  });
 });
